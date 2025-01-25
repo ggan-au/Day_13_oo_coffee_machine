79b3eb37b80d9a05b531bf8306f696a28d2881ca
ai1002hash <glenganderton@proton.me>
2025-01-25 18:06:52 +1100

    Final Commit 2

--- main.py
+++ main.py
@@ -9,10 +9,8 @@ money_machine = MoneyMachine()
 
 while coffee_machine_on:
 
-    #Step 1: Get order for customer
     customer_order = input("What would you like? (espresso/latte/cappuccino/): ").lower()
 
-    #Step 2 & 3: Check of coffee machine was turned off & print report
     if customer_order == "off":
         coffee_machine_on = False
         continue
@@ -20,7 +18,6 @@ while coffee_machine_on:
         print(coffee_machine.report())
         continue
 
-    #Step 4: Check resources are sufficient
     if customer_order in menu.get_items():
         drink = menu.find_drink(customer_order)
         coffee_machine.is_resource_sufficient(drink)
@@ -28,13 +25,10 @@ while coffee_machine_on:
         print("Item not available, try again.")
         continue
 
-
-    #Step 5: Get cost of item
     for item in menu.menu:
         if item.name == customer_order:
             customer_order = item
 
-    #Step 6: Check transation
     if money_machine.make_payment(customer_order.cost):
         coffee_machine.make_coffee(customer_order)
         money_machine.report()
